6eef65e1a6f2e2dfaf9db1f70f9377acbb8614f0
Mdzafar.Imam <mdzafar.imam@intelematics.com>
2019-01-11 10:47:42 +0530

    Fragments added.

---
+++ app/src/main/java/com/aquaheyseller/ui/fragments/HomeFragment.java
@@ -0,0 +1,31 @@
+package com.aquaheyseller.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aquaheyseller.R;
+
+public class HomeFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/aquaheyseller/ui/fragments/OrdersFragment.java
@@ -0,0 +1,30 @@
+package com.aquaheyseller.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aquaheyseller.R;
+
+public class OrdersFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_orders, container, false);
+
+        return view;
+    }
+
+}
