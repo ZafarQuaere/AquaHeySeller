93963ae1287ce287ef16e85f6193f48742a80f10
Mdzafar.Imam <mdzafar.imam@intelematics.com>
2019-01-17 10:07:54 +0530

    Progress Fragment added

--- app/src/main/java/com/aquaheyseller/ui/fragments/OrdersFragment.java
+++ app/src/main/java/com/aquaheyseller/ui/fragments/OrdersFragment.java
@@ -1,6 +1,7 @@
 package com.aquaheyseller.ui.fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,10 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.aquaheyseller.R;
+import com.aquaheyseller.utils.Utils;
 
 public class OrdersFragment extends Fragment {
 
-
+private ProgressFragment progressDialog ;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +25,22 @@ public class OrdersFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_orders, container, false);
+        progressDialog = new ProgressFragment();
+        progressDialog.setCancelable(false);
 
+
+        view.findViewById(R.id.textView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog.showNow(getFragmentManager(),"Progress");
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog.dismiss();
+                    }
+                },5000);
+            }
+        });
         return view;
     }
 
