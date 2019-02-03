93b9e9992c9c653c4d4220576242e1123de97b0e
Zafar Imam <zafarimam512@gmail.com>
2019-02-03 22:11:16 +0530

    orders viewpager added.

---
+++ app/src/main/java/com/aquaheyseller/ui/fragments/CompletedFragment.java
@@ -0,0 +1,56 @@
+package com.aquaheyseller.ui.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aquaheyseller.R;
+
+
+public class CompletedFragment extends Fragment {
+
+
+
+    public CompletedFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_completed, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+    }
+
+}
--- app/src/main/java/com/aquaheyseller/ui/fragments/OrdersFragment.java
+++ app/src/main/java/com/aquaheyseller/ui/fragments/OrdersFragment.java
@@ -1,18 +1,24 @@
 package com.aquaheyseller.ui.fragments;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.aquaheyseller.R;
-import com.aquaheyseller.utils.Utils;
+import com.aquaheyseller.ui.adapters.OrdresPagerAdapter;
+import com.aquaheyseller.utils.LogUtils;
 
-public class OrdersFragment extends BaseFragment {
+public class OrdersFragment extends BaseFragment implements TabLayout.OnTabSelectedListener {
 
+    private Context mContext;
+    private ViewPager viewPager;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,22 +31,47 @@ public class OrdersFragment extends BaseFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_orders, container, false);
-
-
-
-        view.findViewById(R.id.textView).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    openDialog();
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                       hideDialog();
-                    }
-                },5000);
-            }
-        });
+        mContext = getActivity();
+        initUI(view);
         return view;
     }
 
+    private void initUI(View view) {
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
+
+       /* //Adding the tabs using addTab() method
+        tabLayout.addTab(tabLayout.newTab().setText("Pending"));
+        tabLayout.addTab(tabLayout.newTab().setText("Shipped"));
+        tabLayout.addTab(tabLayout.newTab().setText("Completed"));*/
+
+        //tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //Initializing viewPager
+        viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+
+        //Creating our pager adapter
+        OrdresPagerAdapter adapter = new OrdresPagerAdapter(this.getChildFragmentManager(),tabLayout.getTabCount());
+
+        //Adding adapter to pager
+        viewPager.setAdapter(adapter);
+
+        //Adding onTabSelectedListener to swipe views
+        tabLayout.addOnTabSelectedListener(this);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+    }
 }
